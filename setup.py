47d2fa5d13af2237c2b41d37b2ddee1a3e7e40de
Hugo <hugovk@users.noreply.github.com>
2014-09-19 16:09:48 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='osmviz',
     version='1.0',
-    long_description=open('README', 'r').read(),
+    long_description=open('README.md', 'r').read(),
     packages= find_packages('src'),
     package_dir = {'': 'src'},
     zip_safe=False,
