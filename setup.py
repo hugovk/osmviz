659909e82c7f08f0b3dc2f4059eecf7474bdb1bd
Hugo <hugovk@users.noreply.github.com>
2017-04-05 15:54:00 +0300

    pep8 fixes

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='osmviz',
     version='1.0.1',
-    description=
-        'OSMViz is a small set of Python tools for retrieving '
-        'and using Mapnik tiles from a Slippy Map server '
-        '(you may know these as OpenStreetMap images).',
+    description='OSMViz is a small set of Python tools for retrieving '
+                'and using Mapnik tiles from a Slippy Map server '
+                '(you may know these as OpenStreetMap images).',
     long_description=open('README.md', 'r').read(),
     classifiers=[
-        #'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
