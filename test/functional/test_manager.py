cbb90d03b83a495b1c46514a583538f2cfc0d29c
Hugo <hugovk@users.noreply.github.com>
2019-06-18 20:43:44 +0300

    Reduce number of tiles downloaded

--- test/functional/test_manager.py
+++ test/functional/test_manager.py
@@ -3,9 +3,9 @@ import PIL.Image as Image
 
 
 def test_pil():
-    imgr = PILImageManager("RGB")
-    osm = OSMManager(image_manager=imgr)
-    image, bnds = osm.createOSMImage((30, 35, -117, -112), 9)
+    image_manager = PILImageManager("RGB")
+    osm = OSMManager(image_manager=image_manager)
+    image, bounds = osm.createOSMImage((30, 31, -117, -116), 9)
     wh_ratio = float(image.size[0]) / image.size[1]
     image2 = image.resize((int(800 * wh_ratio), 800), Image.ANTIALIAS)
     del image
