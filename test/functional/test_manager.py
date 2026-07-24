721fb22ed972c0a72ea8cd15b61dc1ba08fd24ec
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-07-24 12:27:45 +0300

    Fix RUF059 Unpacked variable 'bounds' is never used

--- test/functional/test_manager.py
+++ test/functional/test_manager.py
@@ -8,7 +8,7 @@ from osmviz.manager import OSMManager, PILImageManager
 def test_pil() -> None:
     image_manager = PILImageManager("RGB")
     osm = OSMManager(image_manager=image_manager)
-    image, bounds = osm.create_osm_image((30, 31, -117, -116), 9)
+    image, _ = osm.create_osm_image((30, 31, -117, -116), 9)
     wh_ratio = float(image.size[0]) / image.size[1]
     image2 = image.resize((int(800 * wh_ratio), 800), Image.Resampling.LANCZOS)
     del image
